20e2fc75210848d1aa65864821c36524bb58be02
github_loomLG <tlomen@blc.edu>
2025-07-14 13:49:38 -0500

    3: Spawn Player and Rotate

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\n"
-          f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     color_black = (0,0,0)
     while True:
@@ -13,7 +14,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color_black)
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
