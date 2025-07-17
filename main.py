3e4111618cea46f1d60479f65f4aab5a5ed98f26
github_loomLG <tlomen@blc.edu>
2025-07-17 14:07:37 -0500

    5: Added Groups, Player added to Groups

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     color_black = (0,0,0)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color_black)
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
